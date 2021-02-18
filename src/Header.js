e8d57ecc59e9affa3de053ce8dfe5777519cf50f
Sune Carelse <sune.carelse@takealot.com>
2021-02-18 21:32:20 +0200

    right side with bell and apps icons / styling

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -22,7 +24,15 @@ function Header() {
         <input placeholder="Search mail" type="text" />
         <ArrowDropDownIcon className="header__inputCaret" />
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </div>
   );
 }
