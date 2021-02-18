73255c92161253ca6bd76863d2bb2c289ce75f0f
Sune Carelse <sune.carelse@takealot.com>
2021-02-18 22:41:02 +0200

    footer icons phone/person/duo

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
@@ -10,6 +10,9 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Sidebar() {
   return (
@@ -32,6 +35,19 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} title="Sent" number={100} />
       <SidebarOption Icon={NoteIcon} title="Drafts" number={100} />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number={100} />
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
