200817d97ec74d0070226254b4e351fbf8811bc8
Sune Carelse <sune.carelse@takealot.com>
2021-02-18 21:04:59 +0200

    header left and middle icons search and menu/style

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 function Header() {
   return (
     <div className="header">
-      <h2>this the Header</h2>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/K-q2WRPRyxxzzPLjxHGt26swMfM=/0x0:1320x880/1200x800/filters:focal(555x335:765x545)/cdn.vox-cdn.com/uploads/chorus_image/image/67587450/newgmaillogo.0.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right"></div>
     </div>
   );
 }
