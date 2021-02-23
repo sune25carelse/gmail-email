7de4432c5e2117ec79234b5b981529ef2bfff199
Sune Carelse <sune.carelse@takealot.com>
2021-02-23 22:09:35 +0200

    mail rows and icons messages descriptn

---
+++ src/EmailRow.js
@@ -0,0 +1,31 @@
+import { Checkbox, IconButton } from "@material-ui/core";
+import React from "react";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+import "./EmailRow.css";
+
+function EmailRow({ time, title, subject, description }) {
+  return (
+    <div className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+      <h3 className="emailRow__title">{title}</h3>
+      <div className="emailRow__message">
+        <h4>
+          {subject}
+          <span className="emailRow__description">{description}</span>
+        </h4>
+      </div>
+      <div className="emailRow__description">{time}</div>
+    </div>
+  );
+}
+
+export default EmailRow;
