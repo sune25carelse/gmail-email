9d6bee93b2960fa43fae68894b6efcc569facdfd
Sune Carelse <sune.carelse@takealot.com>
2021-02-23 21:03:20 +0200

    router-dom installed and mail email list rendered

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
