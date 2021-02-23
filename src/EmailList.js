bb677dac3e5e023a7920af4c572e77a796337bdc
Sune Carelse <sune.carelse@takealot.com>
2021-02-23 22:09:59 +0200

    render mail rows and messages

--- src/EmailList.js
+++ src/EmailList.js
@@ -12,6 +12,7 @@ import Section from "./Section";
 import InboxIcon from "@material-ui/icons/Inbox";
 import PeopleIcon from "@material-ui/icons/People";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import EmailRow from "./EmailRow";
 
 function EmailList() {
   return (
@@ -48,6 +49,15 @@ function EmailList() {
         <Section Icon={InboxIcon} title="primary" color="red" selected />
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
         <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+
+      <div className="emailList__list">
+        <EmailRow
+          title="Twitch"
+          subject="Hey Fellow streamer!!"
+          description="This is Luca's demo"
+          time="10pm"
+        />
       </div>
     </div>
   );
---
+++ src/Mail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Mail.css";
+
+function Mail() {
+  return <div className="mail"></div>;
+}
+
+export default Mail;
