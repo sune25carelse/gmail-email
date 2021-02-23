841199a966cbe44641d41fe31518414243c0010f
Sune Carelse <sune.carelse@takealot.com>
2021-02-23 21:03:56 +0200

    email list settings and icons left/right

---
+++ src/EmailList.js
@@ -0,0 +1,48 @@
+import { Checkbox, IconButton } from "@material-ui/core";
+import React from "react";
+import "./EmailList.css";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import SettingsIcon from "@material-ui/icons/Settings";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+
+function EmailList() {
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections"></div>
+    </div>
+  );
+}
+
+export default EmailList;
