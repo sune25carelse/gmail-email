69dcc986ff71d71395ac9648d04d7937ac765784
Sune Carelse <sune.carelse@takealot.com>
2021-03-01 22:00:24 +0200

    state for opening the compose mail

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
 import SendMail from "./SendMail";
+import { useSelector } from "react-redux";
+import { selectSendMessageIsOpen } from "./features/mailSlice";
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+
   return (
     <Router>
       <div className="app">
@@ -26,7 +30,7 @@ function App() {
           </Switch>
         </div>
 
-        <SendMail />
+        {sendMessageIsOpen && <SendMail />}
       </div>
     </Router>
   );
