9675d4ff7699121ab791babc9127390307019655
Sune Carelse <sune.carelse@takealot.com>
2021-03-01 20:32:25 +0200

    render sendMail for composing mail

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./Sidebar.js";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
+import SendMail from "./SendMail";
 
 function App() {
   return (
@@ -24,6 +25,8 @@ function App() {
             </Route>
           </Switch>
         </div>
+
+        <SendMail />
       </div>
     </Router>
   );
