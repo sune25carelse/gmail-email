28fefb6082c3b0af481373ed4e4e86d40cbe7a3b
Sune Carelse <sune.carelse@takealot.com>
2021-03-02 19:51:47 +0200

    npm i firebase & tools this what connex FE to BE

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2ZbWlRFoJTNDbIuzHkGpIXWRA2qG5rUw",
+  authDomain: "app-29040.firebaseapp.com",
+  projectId: "app-29040",
+  storageBucket: "app-29040.appspot.com",
+  messagingSenderId: "901519389163",
+  appId: "1:901519389163:web:8fd7f14cf4468cd736e9ae",
+  measurementId: "G-24TZ1WJMRM",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
