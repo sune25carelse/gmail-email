620e1739a0620fc24b2421ebb44c468e2caa1473
Sune Carelse <sune.carelse@takealot.com>
2021-03-02 20:10:45 +0200

    dispatch data into db

--- src/SendMail.js
+++ src/SendMail.js
@@ -5,6 +5,8 @@ import { Button } from "@material-ui/core";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { closeSendMessage } from "./features/mailSlice";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function SendMail() {
   const dispatch = useDispatch();
@@ -12,6 +14,14 @@ function SendMail() {
 
   const onSubmit = (formData) => {
     console.log(formData);
+    db.collection("emails").add({
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    dispatch(closeSendMessage());
   };
 
   return (
