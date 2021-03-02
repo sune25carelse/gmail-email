730afb57a16480e66b93cefc528c8e06c26c0db6
Sune Carelse <sune.carelse@takealot.com>
2021-03-02 21:36:45 +0200

    selectds thde mail and open it from db

--- src/Mail.js
+++ src/Mail.js
@@ -13,9 +13,13 @@ import { useHistory } from "react-router-dom";
 import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
 import PrintIcon from "@material-ui/icons/Print";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useSelector } from "react-redux";
+import { selectOpenMail } from "./features/mailSlice";
 
 function Mail() {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
+
   return (
     <div className="mail">
       <div className="mail__tools">
@@ -68,13 +72,13 @@ function Mail() {
       </div>
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
+          <h2>{selectedMail?.subject}</h2>
           <LabelImportantIcon className="mail__important" />
-          <p>Title</p>
-          <p className="mail__time">10pm</p>
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
         <div className="mail__message">
-          <p>this is a message</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
