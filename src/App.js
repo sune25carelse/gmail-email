59f318c83075078a5cf4271da0f4561c4fe4af72
Sune Carelse <sune.carelse@takealot.com>
2021-03-03 20:09:56 +0200

    keep user logged in dont logout

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
 import SendMail from "./SendMail";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
-import { selectUser } from "./features/userSlice";
+import { login, selectUser } from "./features/userSlice";
 import Login from "./Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+  });
 
   return (
     <Router>
