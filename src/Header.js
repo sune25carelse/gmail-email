c9b8fc37d162e35aa1b26b1ab37b2fcc3e79930f
Sune Carelse <sune.carelse@takealot.com>
2021-03-03 20:10:44 +0200

    get  user avatar img and signOut when click on it

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,20 @@ import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function Header() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
+
   return (
     <div className="header">
       <div className="header__left">
@@ -31,10 +43,12 @@ function Header() {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar />
+        <Avatar onClick={signOut} src={user?.photoUrl} />
       </div>
     </div>
   );
 }
 
 export default Header;
+//  now can see my avatar photo
+// also the login and logout from the avatar onclick
